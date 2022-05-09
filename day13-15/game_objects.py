0c2029c4ccf8d14889fd5eba5ce0d2120ea54713
Joel Mann <jmann.creston@gmail.com>
2022-05-09 03:33:58 +0000

    A really bad class based implementation of RPS

--- day13-15/game_objects.py
+++ day13-15/game_objects.py
@@ -1,8 +1,25 @@
 import random
 
+#this is useless, but i misread and thought I was making a DND game, so it stays
 class Dice:
     def __init__(self, number_of_sides: int):
         self.number_of_sides = number_of_sides
     
     def roll(self):
         return random.randint(1,self.number_of_sides)
+
+class Roll: 
+    def __init__(self, name: str, defeats: str):
+        self.name = name
+        self.defeats = defeats
+
+def didPlayerWin(playerRoll: Roll, comparisonRoll: Roll) -> int:
+    if playerRoll.defeats == comparisonRoll.name:
+        print("Player wins this round!")
+        return 1
+    elif comparisonRoll.defeats == playerRoll.name:
+        print("Computer wins this round!")
+        return -1
+    else:
+        print("Tie game!")
+        return 0
--- day13-15/main.py
+++ day13-15/main.py
@@ -1,9 +1,51 @@
-import game_objects
+from game_objects import *
+
+
+def printHeader():
+    print("WELCOME TO ROCK PAPER SCISSORS")
+    print("ya, thats the assignment. Good luck")
+
+
+def getAndValidateInput(dic: dict) -> str:
+    getRoll = input("Roll a 'rock', 'paper', or 'scissors': ").lower()
+    while getRoll not in dic:
+        getRoll = input("Roll a 'rock', 'paper', or 'scissors': ").lower()
+    return getRoll
+
+
+def gameLoop(wins_to_end: int):
+    rolls = {}
+    rolls['rock'] = Roll('rock', 'scissors')
+    rolls['paper'] = Roll('paper', 'rock')
+    rolls['scissors'] = Roll('scissors', 'paper')
+    computerOptions = list(rolls.keys())
+
+    dice = Dice(3)
+    PlayerWins = 0
+    CompWins = 0
+    while max(PlayerWins,CompWins) < wins_to_end:
+        roll = getAndValidateInput(rolls)
+
+        computerRoll = computerOptions[dice.roll()-1]
+        print(f'Computer is playing {computerRoll}')
+
+        round = didPlayerWin(rolls[roll], rolls[computerRoll])
+
+        if round > 0:
+            PlayerWins += 1
+        elif round < 0:
+            CompWins += 1
+        else:
+            continue
+    print(f'Game is over. Score: Player got {PlayerWins} while computer got {CompWins}')
+
 
 def main():
-    d6 = game_objects.Dice(6)
+    printHeader()
+    player = input("Please announce yourself: ")
+    print(f'Welcome to this super exciting game, {player}')
+    gameLoop(3)
 
-    print(d6.roll())
 
 if __name__ == "__main__":
     main()
