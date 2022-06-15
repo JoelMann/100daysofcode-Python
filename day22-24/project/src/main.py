e30505fe3a77e45c31729667ca3de74c37bb3d61
Joel Mann <jmann.creston@gmail.com>
2022-06-15 00:13:10 +0000

    Cleaning up with equivalent code example

--- day22-24/project/src/main.py
+++ day22-24/project/src/main.py
@@ -2,22 +2,35 @@ from functools import wraps
 
 
 def make_html(element: str):
-    '''Decorator that will update plain text with html tags passed in'''
+    """Decorator that will update plain text with html tags passed in"""
+
     def decorator(function):
         @wraps(function)
-        def wrapper(*args,**kwargs):
-            return f'<{element}>{function(*args,**kwargs)}</{element}>'
+        def wrapper(*args, **kwargs):
+            return f"<{element}>{function(*args,**kwargs)}</{element}>"
+
         return wrapper
+
     return decorator
 
-# @make_html("p")
-# @make_html("strong")
 def get_text(text="I code with Pybites"):
+    return text
+
+    
+@make_html("strong")
+@make_html("p")
+def get_text_dec(text="I code with Pybites"):
     return text
 
 
 def main():
-   print(make_html("p")(get_text)("I code with Pybites"))
+    print(
+        make_html("strong")(get_text)((make_html("p")(get_text)("I code with Pybites")))
+    )
+
+    print(get_text_dec())
+
+    # both print <strong><p>I code with Pybites</p></strong>
 
 
 if __name__ == "__main__":
